53e8dabf0cc22b6eedd974bf4423201e17932eb7
M Baba khalil <154895694+BabakhalilM@users.noreply.github.com>
2024-08-29 15:37:29 +0530

    Update api.jsx

--- User-App/src/components/api.jsx
+++ User-App/src/components/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL:"http://localhost:2300/api/",
-  baseURL:"https://user-management-sxwr.onrender.com",
+  baseURL:"https://user-management-sxwr.onrender.com/api",
   
 });
 
